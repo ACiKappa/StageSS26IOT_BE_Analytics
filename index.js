3d1d74b514e3be28fa531e3c58a5af33aed6d350
ACiKappa <katyalazo71@gmail.com>
2026-04-08 11:09:25 +0200

    Initial project setup: Node.js structure, Express server, Sequelize connection

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("StageSS26IOT_BE_Analytics API is running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
